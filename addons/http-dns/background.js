807a98366d533678ec34576be64723ee5198e7d1
Jonathan Kingston <jonathan@jooped.co.uk>
2018-04-17 15:57:45 +0100

    WIP move to states model

--- addons/http-dns/background.js
+++ addons/http-dns/background.js
@@ -1,27 +1,122 @@
 "use strict";
 
 /* global browser */
-
-const PREFS = [
-  {
-    name: "network.trr.mode",
-    type: "int",
-    value: 4
-  },
-  {
-    name: "network.trr.uri",
-    type: "string",
-    value: "https://dns.cloudflare.com/.well-known/dns"
-  }
-];
-const HAS_SEEN_PREF = "network.trr.experimentalRollout";
 const HAS_SEEN_VERSION = 1;
 const STORAGE_AREA = "local";
+
+const stateManager = {
+  hasInit: false,
+
+  async init() {
+    if (!this.hasInit) {
+      const statesFile = browser.runtime.getURL("states.json");
+      const response = await fetch(statesFile);
+      this.statesInfo = await response.json();
+      this.hasInit = true;
+    }
+  },
+
+  async getPrefs(prefNames) {
+    return browser.experiments.rollout.getPrefs(prefNames);
+  },
+
+  async getPref(prefName) {
+    const {[prefName]: prefValue} = await this.getPrefs([prefName]);
+    return prefValue;
+  },
+
+  async getStateName() {
+    await this.init();
+    let currentState = null;
+    const prefValue = await this.getStatePrefValue();
+    Object.keys(this.statesInfo.states).forEach((stateKey) => {
+      const state = this.statesInfo.states[stateKey];
+      if (state.id === prefValue) {
+        currentState = stateKey;
+      }
+    });
+    return currentState;
+  },
+
+  async getStatePrefValue() {
+    await this.init();
+    return this.getPref(this.statesInfo.statePref);
+  },
+
+  async setPrefs(prefs) {
+    return browser.experiments.rollout.setPrefs(prefs);
+  },
+
+  async setState(stateKey) {
+    await this.init();
+    const prefs = [];
+    const state = this.statesInfo.states[stateKey];
+    state.prefs[this.statesInfo.statePref] = state.id;
+    const initial = await this.getIntialPrefValues();
+    const prefNames = await this.getPrefNames();
+    prefNames.forEach((prefName) => {
+      const value = state.prefs[prefName] || initial[prefName] || null;
+      const pref = {
+        name: prefName,
+        value,
+        type: this.statesInfo.prefTypes[prefName]
+      };
+      prefs.push(pref);
+    });
+    return this.setPrefs(prefs);
+  },
+
+  getUserPrefKey(key) {
+    return `userPref_${key}`;
+  },
+
+  async getPrefNames() {
+    await this.init();
+    return Object.keys(this.statesInfo.prefTypes);
+  },
+
+  async getIntialPrefValues() {
+    const prefNames = await this.getPrefNames();
+    const storePrefNames = prefNames.map(n => {
+      return this.getUserPrefKey(n);
+    });
+    const initialStoredValues = await browser.storage[STORAGE_AREA].get(storePrefNames);
+    const initialValues = {};
+    prefNames.forEach(prefName => {
+      initialValues[prefName] = initialStoredValues[this.getUserPrefKey(prefName)];
+    });
+    return initialValues;
+  },
+
+  /**
+   * Get the initial user values of the experiments preferences and store them into the extension
+   */
+  async setInitialUserPrefValues() {
+    const prefNames = await this.getPrefNames();
+    const prefValues = await this.getPrefs(prefNames);
+    const storeValues = {};
+    Object.keys(prefValues).forEach((key) => {
+      storeValues[this.getUserPrefKey(key)] = prefValues[key];
+    });
+    return browser.storage[STORAGE_AREA].set(storeValues);
+  },
+};
 
 const rollout = {
   async init() {
     browser.runtime.onMessage.addListener((...args) => this.handleMessage(...args));
-    await this.show();
+    const stateName = await stateManager.getStateName();
+    switch (stateName) {
+      case null:
+      case "loaded":
+        await stateManager.setInitialUserPrefValues();
+        await stateManager.setState("loaded");
+        await this.show();
+        break;
+      case "enabled":
+      case "disabled":
+        break
+    }
   },
 
   handleMessage(message) {
@@ -32,64 +127,8 @@ const rollout = {
     }
   },
 
-  async getPrefs() {
-    return browser.experiments.rollout.getPrefs(this.getPrefNames());
-  },
-
-  getPrefNames() {
-    return PREFS.map((pref) => {
-      return pref.name;
-    });
-  },
-
-  enable() {
-    return this.setPrefs(PREFS);
-  },
-
-  getUserPrefKey(key) {
-    return `userPref_${key}`;
-  },
-
-  /**
-   * Get the initial user values of the experiments preferences and store them into the extension
-   */
-  async storeInitialUserPrefValues() {
-    const prefValues = await this.getPrefs();
-    const storeValues = {};
-    Object.keys(prefValues).forEach((key) => {
-      const val = prefValues[key];
-      if (val !== null) {
-        storeValues[this.getUserPrefKey(key)] = val;
-      }
-    });
-    return browser.storage[STORAGE_AREA].set(storeValues);
-  },
-
-  /**
-   * Reset extension prefs to initial values
-   */
-  async resetToInitial() {
-    const prefNames = this.getPrefNames();
-    const storePrefNames = prefNames.map(n => {
-      return this.getUserPrefKey(n);
-    });
-    const initialValues = await browser.storage[STORAGE_AREA].get(storePrefNames);
-    /* map PREFS object to contain null / initial value which will reset to what it was pre extension */
-    return this.setPrefs(PREFS.map((pref) => {
-      pref.value = initialValues[this.getUserPrefKey(pref.name)] || null;
-      return pref;
-    }));
-  },
-
-  /**
-   * Set browser preferences to prefs objects
-   */
-  async setPrefs(prefs) {
-    return browser.experiments.rollout.setPrefs(prefs);
-  },
-
   async disable() {
-    await this.resetToInitial();
+    await stateManager.setState("disabled");
     const url = this.introUrl;
     const tabs = await browser.tabs.query({url});
     return browser.tabs.remove(tabs.map(({id}) => id));
@@ -99,32 +138,12 @@ const rollout = {
     return browser.runtime.getURL("intro.html");
   },
 
-  async getHasSeenPref() {
-    const {[HAS_SEEN_PREF]:setupPref} = await browser.experiments.rollout.getPrefs([HAS_SEEN_PREF]);
-    return setupPref;
-  },
-
-  async setHasSeenPref(value) {
-    return this.setPrefs([
-      {
-        name: HAS_SEEN_PREF,
-        value,
-        type: "int"
-      }
-    ]);
-  },
-
   async show() {
-    const hasSeenPref = await this.getHasSeenPref();
-    if (!hasSeenPref || hasSeenPref < HAS_SEEN_VERSION) {
-      await this.storeInitialUserPrefValues();
-      await this.enable();
-      await this.setHasSeenPref(1);
-      const url = this.introUrl;
-      browser.tabs.create({
-        url
-      });
-    }
+    await stateManager.setState("enabled");
+    const url = this.introUrl;
+    browser.tabs.create({
+      url
+    });
   }
 };
 
